5ec1c17069084d5647250f069b95fd9a97300b86
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-02-21 05:59:11 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
+require 'pry'
 def unsafe?(speed)
-
+# binding.pry
+if speed > 60
+  true
+  elsif speed < 40
+  true
+  elsif speed >= 40 && speed <= 60
+  false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed >= 40 && speed <= 60) ? false : true
 end
 	
 
