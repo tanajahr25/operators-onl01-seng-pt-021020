2225249b948ff36871d0eca1d7ec8f69a2a17d2c
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-02-21 05:15:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,15 @@
+require 'pry'
 def unsafe?(speed)
-
+# binding.pry
+if speed > 60
+  true
+  elsif speed < 40
+  true
+  else 40 <= speed >= 40
+    binding.pry 
+    speed >= 40 && speed <= 60
+  false
+  end
 end
 
 
