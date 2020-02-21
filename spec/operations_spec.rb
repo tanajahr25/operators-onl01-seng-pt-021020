55088651b1d091890d288d37004493454b4fd0bd
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-02-21 05:40:54 +0000

    Automatically backed up by Learn

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -5,34 +5,34 @@ describe "Operations" do
   describe "#unsafe?" do
     it 'returns true if speed is greater than 60' do
       answer = unsafe?(79)
-      expect(answer).to eq('true')
+      expect(answer).to eq(true)
     end
 
     it 'returns true if speed is less than 40' do
       answer = unsafe?(35)
-      expect(answer).to eq('true')
+      expect(answer).to eq(true)
     end
 
     it 'returns false if the speed is between 40 and 60' do
       answer = unsafe?(50)
-      expect(answer).to eq('false')
+      expect(answer).to eq(false)
     end
   end
 
   describe "#not_safe?" do
     it 'returns true if speed is greater than 60' do
       answer = not_safe?(79)
-      expect(answer).to eq('true')
+      expect(answer).to eq(true)
     end
 
     it 'returns true if speed is less than 40' do
       answer = not_safe?(35)
-      expect(answer).to eq('true')
+      expect(answer).to eq(true)
     end
 
     it 'returns false if the speed is between 40 and 60' do
       answer = not_safe?(50)
-      expect(answer).to eq('false')
+      expect(answer).to eq(false)
     end
 
     it 'uses the ternary operator' do
